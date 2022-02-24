b4836958d318ce971d7ae29ed6af7e10f7b45367
fmul <fmul@fresnou.org>
2022-02-24 08:53:46 -0800

    initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const {User} = require('../models');
+const md5 = require('md5');
+const passport=require('passport');
+module.exports.renderRegistration =function (req , res ){
+    res.render('users/register');
+};
+
+module.exports.register = async function(req,res){
+    const existingUser = await User.findOne({
+        where: {
+            email:req.body.email
+        }
+    });
+    if (existingUser){
+        res.render('users/register', {
+            error: 'User Already Exists'
+        })
+    } else {
+        await User.create({
+            email: req.body.email,
+            password:md5(req.body.password),
+            first_name:req.body.first_name,
+            last_name:req.body.last_name
+    });
+    res.redirect('/');
+    }
+}
+
+
+module.exports.renderLogin = function (req,res){
+    let error = null
+    if(req.session.message && req.session.message.length>0){
+        error = req.session.message[0];
+    }
+    res.render('users/login',{error});
+}
+
+module.exports.authenticate = passport.authenticate('local',{
+    successRedirect:'/',
+    failureRedirect:'/login',
+    failureMessage:true
+});
